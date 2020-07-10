17e405abdbbf219f792f9ca217aff9185f84c76f
Zidan M. Daffa <zidandaff@gmail.com>
2020-07-10 21:50:07 +0700

    init

---
+++ asset/script/script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.toggle');
+const mobileMenu = document.querySelector('nav ul');
+const close = document.querySelector(".close");
+
+toggleBtn.addEventListener("click", () => {
+    mobileMenu.classList.add('slide')
+})
+
+close.addEventListener('click', ()=> {
+    mobileMenu.classList.remove("slide")
+})
