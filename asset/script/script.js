c9aa0855c55fb923287503c76e90e7a335129faf
Zidan M. Daffa <zidandaff@gmail.com>
2020-09-30 15:07:39 +0700

    delete file

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.querySelector('.toggle');
 const mobileMenu = document.querySelector('nav ul');
 const close = document.querySelector(".close");
+const switchBtn = document.querySelector(".swicth")
 
 toggleBtn.addEventListener("click", () => {
     mobileMenu.classList.add('slide')
@@ -9,3 +10,7 @@ toggleBtn.addEventListener("click", () => {
 close.addEventListener('click', ()=> {
     mobileMenu.classList.remove("slide")
 })
+
+// switchBtn.addEventListener("click", ()=> {
+//     if()
+// })
